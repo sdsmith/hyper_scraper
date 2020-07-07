b750924af3bd36b01a6ca45672abf028b035e694
Stewart Smith <stewart.dryden.smith@gmail.com>
2020-07-07 04:56:45 -0400

    Add current stock check

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
+import sys
 from scrapy.crawler import CrawlerProcess
 from hyper_scraper.spiders.walmart_spider import WalmartNintendoSwitchSpider
 from db.dao import Dao
 
+
 if __name__ == '__main__':
     Dao.setup_db()
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'stock':
+            Dao.products_in_stock()
+            exit(0)
+        else:
+            print('Usage: main.py [stock]')
 
     process = CrawlerProcess()
     process.crawl(WalmartNintendoSwitchSpider)
